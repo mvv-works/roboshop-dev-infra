afc14dc7985704075984dfc2990be0daae39b6fe
vamsi <vijayvamsi550@gmail.com>
2026-07-20 10:53:11 +0530

    roboshop-dev-infra

---
+++ 40-databases/variables.tf
@@ -0,0 +1,19 @@
+variable "project" {
+  default = "roboshop"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "zone_id" {
+  default = "Z0854017A579CB8A4CIY"
+}
+
+variable "domain_name" {
+  default = "devops550.online"
+}
+
+variable "mysql_root_password" {
+  type = string
+}
