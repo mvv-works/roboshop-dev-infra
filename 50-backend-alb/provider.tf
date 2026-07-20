ac8ded8b88904496135377e49a13adafe708a318
vamsi <vijayvamsi550@gmail.com>
2026-07-20 15:34:45 +0530

    roboshop-dev-infra

---
+++ 50-backend-alb/provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.48.0"
+    }
+  }
+
+  # remote state storage location
+  backend "s3" {
+    bucket       = "remote-state-55-dev"
+    key          = "roboshop-backend_alb.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true # Enables native S3 locking (Terraform 1.10+)
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
