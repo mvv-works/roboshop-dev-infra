ab176a991ec291e8899648553951a9223f90095b
vamsi <vijayvamsi550@gmail.com>
2026-07-23 12:29:32 +0530

    roboshop-dev-infra

---
+++ 70-acm/variables.tf
@@ -0,0 +1,16 @@
+variable "project" {
+  default = "roboshop"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "domain_name" {
+  default = "devops550.online"
+}
+
+variable "zone_id" {
+  default = "Z0854017A579CB8A4CIY"
+}
+
